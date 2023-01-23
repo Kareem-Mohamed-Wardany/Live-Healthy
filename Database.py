9e7fbec059ab7a917cb2317baf9c710181da81aa
Kareem-Mohamed-Wardany <kareemwardany1111@gmail.com>
2023-01-23 22:30:58 +0200

    File Updated

--- Database.py
+++ Database.py
@@ -1,55 +1,27 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="mydatabase"
-)
-print(mydb)
 
-# Insert Query
+class Database:
+    # Database connection String
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="Systemdb"
+    )
+    def Update(self, Query, Values):
+        mycursor = self.mydb.cursor()
+        mycursor.execute(Query, Values)
 
-# mycursor = mydb.cursor()
-# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-# val = ("John", "Highway 21")
-# mycursor.execute(sql, val)
-# mydb.commit()
-# print(mycursor.rowcount, "record inserted.")
+    def Select(self, Query, Values = []):
+        mycursor = self.mydb.cursor()
+        mycursor.execute(Query,Values)
+        myresult = mycursor.fetchall()
+        return myresult
 
-
-
-# Select Query
-
-# mycursor = mydb.cursor()
-# mycursor.execute("SELECT * FROM customers")
-# myresult = mycursor.fetchall()
-# for x in myresult:
-#   print(x)
-
-
-# Select Query with where
-
-# mycursor = mydb.cursor()
-# sql = "SELECT * FROM customers WHERE address ='Park Lane 38'"
-# mycursor.execute(sql)
-# myresult = mycursor.fetchall()
-# for x in myresult:
-#   print(x) 
-
-# Update Query 
-
-# mycursor = mydb.cursor()
-# sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'"
-# mycursor.execute(sql)
-# mydb.commit()
-# print(mycursor.rowcount, "record(s) affected")
-
-# Update with condition
-
-# mycursor = mydb.cursor()
-# sql = "UPDATE customers SET address = %s WHERE address = %s"
-# val = ("Valley 345", "Canyon 123")
-# mycursor.execute(sql, val)
-# mydb.commit()
-# print(mycursor.rowcount, "record(s) affected")
+    def Insert(self, Query, Values = []):
+        mycursor = self.mydb.cursor()
+        mycursor.execute(Query,Values)
+        
+    def Commit(self):
+        self.mydb.commit()
