4f569f77eacab5ee9d604e1a473f92df0cb5a109
Kareem-Mohamed-Wardany <kareemwardany1111@gmail.com>
2023-02-05 16:29:24 +0200

    Main Files

---
+++ test.py
@@ -0,0 +1,42 @@
+from Database import *
+BloodTypes = ["O-","O+","B-","B+","A-","A+","AB-","AB+"]
+db = Database()
+
+import queue
+
+binaryImage = db.convertToBinaryData("COVID-996.png")
+
+# qu = queue.Queue()
+
+# qu.put("Ali: Hello Doctor")
+# qu.put("Amira: Hello Ali, How are you")
+# qu.put("Ali: Feeling ill")
+
+# print(qu.queue)
+# c = list(qu.queue)
+
+
+# textChat =""
+# for i in range(len(c)):
+#     if i == 0:
+#         textChat += c[i]
+#     else:
+#         textChat += "&,&"+c[i] 
+# res = db.Update("UPDATE requests SET X_ray_scan= %s, Prediction= %s WHERE Patient_ID= %s",[binaryImage,"Covid-19",7])
+
+
+# print(textChat)
+
+# symp = "Fever, "+ "Cough, "+"difficulty breathing, "+"Fatigue, "+"Headache, "+"loss of taste and smell, " + "Sore throat"
+# # print(symp)
+id = 5
+res1 = db.Update("UPDATE requests SET Request_Status= %s WHERE Patient_ID= %s",["ongoing", id])
+res2 = db.Update("UPDATE chatdata SET Chat_Status= %s WHERE Patient_ID= %s",["ongoing", id])
+db.Commit()
+
+
+
+# db.write_file(binaryImage,"test12.pdf")
+
+
+# print(binaryImage)
