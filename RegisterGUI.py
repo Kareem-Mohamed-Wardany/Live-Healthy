5f4444060bac988999cc88bb57679d38373c286e
karim-tarekk <karimtarek2013@gmail.com>
2023-02-08 17:32:57 +0200

    Update

--- RegisterGUI.py
+++ RegisterGUI.py
@@ -16,7 +16,7 @@ class Register(ctk.CTk):
 
     def __init__(self):
         super().__init__()
-        Title = "Login"
+        Title = "Register"
         self.title(Title)
         # set Dimension of GUI
         self.geometry(self.config.get("FramesSize"))
@@ -30,10 +30,13 @@ class Register(ctk.CTk):
             font=ctk.CTkFont(size=30, weight="bold"),
         )
         label.place(anchor="nw", relx=0.01, rely=0.01)
+        l = ctk.CTkEntry(self,placeholder_text="Test")
+        # n, ne, e, se, s, sw, w, nw, or center
+        l.place(anchor="nw",relx=0,rely=0)
 
 
 if __name__ == "__main__":
     app = Register()
     app.resizable(False, False)  # Disable resize for GUI
-    center(app)  # center window in your screen
+    # center(app)  # center window in your screen
     app.mainloop()
