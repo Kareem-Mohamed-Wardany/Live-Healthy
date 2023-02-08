f4ba315f0b303c315b486e2af2b2c6d15b2c86a6
Kareem-Mohamed-Wardany <kareemwardany1111@gmail.com>
2023-02-08 17:55:38 +0200

    Update

--- GUIHelperFunctions.py
+++ GUIHelperFunctions.py
@@ -20,16 +20,24 @@ def center(win):
     centers a tkinter window
     :param win: the main window or Toplevel window to center
     """
-    win.update_idletasks()
-    width = win.winfo_width()
-    frm_width = win.winfo_rootx() - win.winfo_x()
-    win_width = width + 2 * frm_width
-    height = win.winfo_height()
-    titlebar_height = win.winfo_rooty() - win.winfo_y()
-    win_height = height + titlebar_height + frm_width
-    x = win.winfo_screenwidth() // 2 - win_width // 2
-    y = win.winfo_screenheight() // 2 - win_height // 2
-    win.geometry(f"{width}x{height}+{x}+{y}")
+    w = win.winfo_width()
+    h = win.winfo_height()
+    ws = win.winfo_screenwidth()
+    hs = win.winfo_screenheight()
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+
+    win.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    # win.update_idletasks()
+    # width = win.winfo_width()
+    # frm_width = win.winfo_rootx() - win.winfo_x()
+    # win_width = width + 2 * frm_width
+    # height = win.winfo_height()
+    # titlebar_height = win.winfo_rooty() - win.winfo_y()
+    # win_height = height + titlebar_height + frm_width
+    # x = win.winfo_screenwidth() // 2 - win_width // 2
+    # y = win.winfo_screenheight() // 2 - win_height // 2
+    # win.geometry(f"{width}x{height}+{x}+{y}")
     win.deiconify()
 
 
